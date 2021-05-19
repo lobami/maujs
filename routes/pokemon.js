746e017dc07a67559a9bd1ac9cbe2cd0040887d6
Loth Mejia martinez <eljugador360@gmail.com>
2021-05-18 20:03:55 -0500

    ACT 8 & 9

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -4,8 +4,39 @@ const pokemon = express.Router();
 const db = require('../config/database')
 
 
-pokemon.post("/", (req, res, next) =>{
-    return res.status(201).send(req.body)
+pokemon.post("/", (req, res, next) => {
+    const { pok_name, pok_height, pok_weight, pok_base_experience } = req.body;
+    if (pok_name && pok_height && pok_weight && pok_base_experience) {
+        let query = `INSERT INTO pokemon (pok_name, pok_height, pok_weight, pok_base_experience) VALUES ('${pok_name}', '${pok_height}', '${pok_weight}', '${pok_base_experience}')`;
+        const rows = await db.query(query)
+        if (rows.affectedRows == 1){
+            return res.status(200).json({ code: 200, message: "pokemon creado correctamente"})
+        }
+        return res.status(404).json({ code: 404, message: "pokemon no creado"})
+    }
+})
+
+pokemon.delete("/:id([0-9]{1,3}", (req, res, next) =>{
+    const id = req.params.id
+    const query_delete = "DELETE FROM pokemon WHERE pok_id=" + id;
+    const rows = await db.query(query_delete);
+    if (rows.affectedRows == 1){
+        return res.status(200).json({ code: 200, message: "pokemon borrado correctamente"})
+    }
+    return res.status(404).json({ code: 404, message: "pokemon no encontrado"})
+})
+
+pokemon.put("/:id([0-9]{1,3}", (req, res, next) =>{
+    const { pok_name, pok_height, pok_weight, pok_base_experience } = req.body;
+    if (pok_name && pok_height && pok_weight && pok_base_experience) {
+        let query = `UPDATE pokemon SET pok_name='${pok_name}', pok_height='${pok_height}', `;
+        query += `pok_weight='${pok_weight}', pok_base_experience=${pok_base_experience}, WHERE pok_id=${pok_id}`;
+        const rows = await db.query(query)
+        if (rows.affectedRows == 1){
+            return res.status(200).json({ code: 200, message: "pokemon actualizado correctamente"})
+        }
+        return res.status(404).json({ code: 404, message: "pokemon no creado"})
+    }
 })
 
 pokemon.get("/", async(req, res, next) =>{
