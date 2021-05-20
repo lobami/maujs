95cce96cd20382859f21338671fd107a34224e95
Loth Mejia martinez <eljugador360@gmail.com>
2021-05-19 20:03:22 -0500

    ADD middlewares

--- index.js
+++ index.js
@@ -4,24 +4,23 @@ const express = require('express')
 const app = express()
 const pokemon = require ('./routes/pokemon')
 const user = require ('./routes/user')
+const auth = require ('./middleware/auth')
+const notFound = require ('./middleware/notFound')
+const wellcome = require ('./middleware/wellcome')
 
 app.use(morgan('dev'));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
-/*app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}))*/
 
-app.get("/", (req, res, next) =>{
-    return res.status(200).json({ code: 1, message: 'bienvenido, "crack"'})
-})
 
+app.get("/", wellcome)
+
+app.get(auth)
 
 app.use("/pokemon", pokemon);
 app.use("/user", user)
 
-app.use((req, res, next) => {
-    return res.status(404).json({code: 404, message: "url no encontrada"})
-})
+app.use(notFound)
 
 app.listen(process.env.PORT || 3000, () =>{
     console.log('El servidor esta corriendo...');
